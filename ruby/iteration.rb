aeafa806943f186998a6da12f66dcbc8e9bd763f
Edward Kim <edwardjkim@edwards-air-3.attlocal.net>
2016-04-21 14:20:35 -0700

    Update iteration file

---
+++ ruby/iteration.rb
@@ -0,0 +1,11 @@
+def say_hello
+  name1 = "Steve"
+  name2 = "Tiahna"
+  puts "Why, hello there!"
+  yield(name1, name2)
+end
+
+say_hello { |name1, name2| puts "Great to see you, #{name1} and #{name2}!" }
+
+
+nba_players = ["Steph Curry", "Kobe Bryant", "Lebron James", "Derrick Rose"]
