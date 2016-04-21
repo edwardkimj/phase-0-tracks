180189ba2a4ffc72b4fba582de99163cbb4db80d
Edward Kim <edwardjkim@edwards-air-3.attlocal.net>
2016-04-21 14:48:10 -0700

    Update iteration file with release 1 and 2

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,11 +1,11 @@
-# def say_hello
-#   name1 = "Steve"
-#   name2 = "Tiahna"
-#   puts "Why, hello there!"
-#   yield(name1, name2)
-# end
-#
-# say_hello { |name1, name2| puts "Great to see you, #{name1} and #{name2}!" }
+def say_hello
+  name1 = "Steve"
+  name2 = "Tiahna"
+  puts "Why, hello there!"
+  yield(name1, name2)
+end
+
+say_hello { |name1, name2| puts "Great to see you, #{name1} and #{name2}!" }
 
 
 nba_players = ["Steph Curry", "Kobe Bryant", "Lebron James", "Derrick Rose"]
@@ -25,3 +25,51 @@ nba_players.map! do |name|
   name + "!!"
 end
 p nba_players
+
+array_num = [1, 2, 3, 4, 5]
+
+Arrays
+
+array_num.delete_if do |num|
+  num < 5
+end
+p array_num
+
+array_num.keep_if do |num|
+  num < 5
+end
+p array_num
+
+array1 = array_num.take_while do |num|
+  num < 5
+end
+p array1
+
+array2 = array_num.drop_while do |num|
+  num < 5
+end
+p array2
+
+hash_num = { one: 1, two: 2, three: 3, four: 4, five: 5}
+
+Hashes
+
+hash_num.delete_if do |word, num|
+  num < 3
+end
+p hash_num
+
+hash_num.keep_if do |word, num|
+  num < 3
+end
+p hash_num
+
+hash_num.select! do |word, num|
+  num < 3
+end
+p hash_num
+
+hash_num.reject! do |word, num|
+  num < 3
+end
+p hash_num
