2e8257e46c9ed1b67bb8a6f28c207d4101c6c619
Edward Kim <edwardjkim@Edwards-MacBook-Air-3.local>
2016-04-21 11:14:17 -0700

    update arrays drill file with release 2

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,3 +1,15 @@
+def build_array(arg1, arg2, arg3 )
+    array = [arg1, arg2, arg3]
+end
+p build_array("first", "second", "third")
+p build_array(3, 5, "name")
+
+def add_to_array(arr, value)
+  arr << value
+end
+p add_to_array([], 1)
+p add_to_array(["hello", 3, true], "edward")
+
 empty_array = []
 p empty_array
 
