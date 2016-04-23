0bbee8544c713373440bedc972695aa2650a04ce
Edward Kim <edwardjkim@edwards-air-3.attlocal.net>
2016-04-22 20:21:29 -0700

    Update method detective file with code

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -31,9 +31,9 @@
 “Elementary  ,    my   dear      Watson!”.squeeze
 # => “Elementary, my dear Watson!”
 
-“z”.
+“z”.bytes
 # => 122
 # (What is the significance of the number 122 in relation to the character z?)
 
-“How many times does the letter ‘a’ appear in this string?”.<???>
+“How many times does the letter ‘a’ appear in this string?”.count "a"
 #=> 4
