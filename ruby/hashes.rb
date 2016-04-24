4cf69c5fce2a40c51a8542367f98294cd076c965
Edward Kim <edwardjkim@edwards-air-3.attlocal.net>
2016-04-24 15:50:56 -0700

    Add hashes file

---
+++ ruby/hashes.rb
@@ -0,0 +1,21 @@
+# applicant = {
+#   name: "Anishka Clarke",
+#   address: "1451 Broadway Avenue, New York, NY",
+#   email: "anishka@interiordesigngeniuses.com",
+#   phone: nil,
+#   fave_blue: "indigo",
+#   likes_paisley: true,
+#   likes_chevrons: false,
+#   likes_photorealistic_woods: false,
+#   likes_abstract_woods: true,
+#   ombre: "Practically medieval in its appalling irrelevance"
+# }
+
+# First create a hash on information on the client
+ client = {
+   name: "John Smith"
+   age: 26
+   number_children: 4
+   decor_theme: "beach"
+   
+ }
