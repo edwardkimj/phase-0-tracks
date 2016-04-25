ad5a43adec2c8d625b1906dc763979e7a0293b00
Edward Kim <edwardjkim@edwards-air-3.attlocal.net>
2016-04-24 23:19:59 -0700

    Update nested data file with release 2

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,11 +1,46 @@
 classroom = {
   english: {
-    
-  }
+    topic: 'grammar',
+    seat_info: {
+      total_seats: 30,
+      seats_available: 4
+    },
+    supplies: [
+      "pens",
+      "paper",
+      "books"
+    ]
+  },
   math: {
-
-  }
+    topic: 'calculus',
+    seat_info: {
+      total_seats: 32,
+      seats_available: 5
+    },
+    supplies: [
+      "calculator",
+      "pencil",
+      "ruler"
+    ]
+  },
   history: {
-
+    topic: 'US History',
+    seat_info: {
+      total_seats: 25,
+      seats_available: 10
+    },
+    supplies: [
+      "paper",
+      "books",
+      "notebook"
+    ]
   }
 }
+
+p classroom[:english][:topic]
+
+p classroom[:math][:supplies]
+
+p classroom[:math][:supplies][0]
+
+p classroom[:history][:supplies].push("pens")
