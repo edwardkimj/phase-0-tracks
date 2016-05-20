1dabae50f9dbd3c82adc9c251e0284069cde369d
Edward Kim <edwardjkim@edwards-air-3.attlocal.net>
2016-05-20 11:10:06 -0700

    Add puppy 2 file and update with comments

---
+++ ruby/puppy2.rb
@@ -0,0 +1,15 @@
+# SPECIES ----------------
+# Golden Retriever
+
+# CHARACTERISTICS ---------------
+# Age: 2 (human years)
+# Friendly: yes
+# Name: Lance
+# Weight: 80 lbs
+# Color: Golden Brown
+
+# BEHAVIOR ------------
+# Jump
+# Lick
+# Fetch
+# Run
