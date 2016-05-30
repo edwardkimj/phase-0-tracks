f47cf83668b9116e79d364c2f675e262f93172a3
Edward Kim <edwardjkim@edwards-air-3.attlocal.net>
2016-05-30 08:05:57 -0700

    Add and update exlpore file with beginning code

---
+++ js/explore.js
@@ -0,0 +1,9 @@
+// REVERSE pseudocode
+// 1. Write a function called reverse that takes in a string as a parameter
+// 2. name an empty variable reverse_string and make it equal to ""
+// 3. use a for loop to loop through each letter of the string but start your
+// counter from the last letter of the string by using string.length and
+// increment backwards by using i
+// 4. While using the for loop we created, push the results into your empty var
+// reverse_string.
+// 5. console.log reverse_string and end the function
