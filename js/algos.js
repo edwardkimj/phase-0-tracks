4177de2c59f6c270eda8161898295b361d650e58
Edward Kim <edwardjkim@Edwards-MacBook-Air-3.local>
2016-05-31 10:34:11 -0700

    Update algos file with psuedocode

---
+++ js/algos.js
@@ -0,0 +1,7 @@
+// Release 0: Find the longest Phrase
+// Start by declaring a function by the name of longest_word that takes an
+// array as a parameter
+// To recieve the longest word in a set of arrays, we need to iterate through the array
+// and print out the longest word
+// When iterating through the array, compare the first word with the other words
+// or phrases 
